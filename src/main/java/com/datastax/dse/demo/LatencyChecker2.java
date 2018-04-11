c16070fb473448a66d6e88923fb4b5c689fe584d
Subrata Dasgupta <subratad@gmail.com>
2018-04-11 15:51:34 -0700

    testing

--- src/main/java/com/datastax/dse/demo/LatencyChecker2.java
+++ src/main/java/com/datastax/dse/demo/LatencyChecker2.java
@@ -117,6 +117,8 @@ public class LatencyChecker2 {
 
         if (authMethod.compareTo("username_password") == 0) {
 
+            System.out.println("Before cluster1 definiton\n");
+
             //For DC #1
             cluster1 = Cluster.builder()
                     .addContactPoints(CONTACT_POINTS_DC1).withPort(PORT)
@@ -128,6 +130,8 @@ public class LatencyChecker2 {
                     ).withQueryOptions(new QueryOptions().setConsistencyLevel(CONSISTENCY))
                     .build();
 
+            System.out.println("Before cluster2 definiton\n");
+
             //For DC #2
             cluster2 = Cluster.builder()
                     .addContactPoints(CONTACT_POINTS_DC2).withPort(PORT)
@@ -138,6 +142,8 @@ public class LatencyChecker2 {
                                     .build()
                     ).withQueryOptions(new QueryOptions().setConsistencyLevel(CONSISTENCY))
                     .build();
+
+            System.out.println("After cluster2 definiton\n");
         }
         else if (authMethod.compareTo("kerberos") == 0) {
 
